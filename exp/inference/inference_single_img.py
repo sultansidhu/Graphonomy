3c4b92c549444effc5d08a09ec98441a892f00c1
rohit_saha <rohit@modiface.com>
2020-11-04 18:54:01 +0000

    guided filter

--- exp/inference/inference_single_img.py
+++ exp/inference/inference_single_img.py
@@ -19,10 +19,13 @@ import cv2
 # Custom includes
 from networks import deeplab_xception_transfer, graph
 from dataloaders import custom_transforms as tr
-
+from guided_filter import GuidedFilter
 #
 import argparse
 import torch.nn.functional as F
+
+
+
 
 label_colours = [
     (0, 0, 0),
@@ -115,7 +118,7 @@ def img_transform(img, transform=None):
     return sample
 
 
-def inference(net, img_path="", output_path="./", output_name="f", use_gpu=True):
+def inference(net, img_path="", output_path="./", use_gpu=True):
     """
 
     :param net:
@@ -139,8 +142,9 @@ def inference(net, img_path="", output_path="./", output_name="f", use_gpu=True)
 
     # multi-scale
     scale_list = [1, 0.5, 0.75, 1.25, 1.5, 1.75]
-    img = read_img(img_path)
-    img = img.resize((256, 256))
+    real_img = read_img(img_path)
+
+    img = real_img.resize((256, 256))
     testloader_list = []
     testloader_flip_list = []
     for pv in scale_list:
@@ -201,11 +205,6 @@ def inference(net, img_path="", output_path="./", output_name="f", use_gpu=True)
             else:
                 outputs_final = outputs.clone()
 
-    foreground = outputs_final[0, 1:, :, :] # [19 feature maps]
-    foreground = torch.sum(foreground, axis=0) # [256, 256]
-    H, W = foreground.shape
-    foreground_unroll = torch.flatten(foreground)
-    foreground_unroll = nn.Softmax(dim=0)(foreground_unroll)
     #foreground_unroll = torch.resize(foreground_unroll, (H, W))
     
     ################ plot pic
@@ -214,7 +213,7 @@ def inference(net, img_path="", output_path="./", output_name="f", use_gpu=True)
     vis_res = decode_labels(results)
 
     parsing_im = Image.fromarray(vis_res[0])
-    parsing_im.save(os.path.join(output_path, output_name))
+    parsing_im.save(os.path.join(output_path, 'real_mask.png'))
     #cv2.imwrite("outputs/{}_gray.png".format(output_name), results[0, :, :])
 
     end_time = timeit.default_timer()
@@ -223,6 +222,52 @@ def inference(net, img_path="", output_path="./", output_name="f", use_gpu=True)
         + " is :"
         + str(end_time - start_time)
     )
+
+
+    # guided filter code goes here
+    guidance_map = real_img.resize((512, 512))
+    guidance_map = transforms.ToTensor()(guidance_map)
+    guidance_map = guidance_map.unsqueeze(0).cuda()
+    
+    guide = GuidedFilter(
+        #guidance_map.shape[-2:],
+        r=16,
+        eps=1e-3,
+        downsample_stride=4,
+    ).cuda()
+    guide.conv_mean_weights = guide.conv_mean_weights.cuda()
+
+    predictions = F.interpolate(
+        outputs_final,
+        size=guidance_map.shape[-2:],
+        mode='bilinear',
+        align_corners=False
+    )
+
+    # get guided predictions; 
+    # guidance_map: [1, 3, 512, 512], predictions: [1, 20, 512, 512]
+    for idx, channel in enumerate(predictions[0, ...]):
+        current_channel = predictions[:, idx, :, :]
+        current_channel = torch.unsqueeze(current_channel, axis=0) # [1, 1, 512, 512]
+        if idx == 0:
+            guided_predictions = guide(guidance_map, current_channel) # [1, 1, 512, 512]
+        else:
+            guided_predictions = torch.cat(
+                (guided_predictions, guide(guidance_map, current_channel)),
+                axis=1
+            )
+    # guided_predictions: [1, 20, 512, 512]
+    guided_predictions = F.softmax(guided_predictions, dim=1)
+    foreground_predictions = guided_predictions[0, 1:, ...] # [19, 512, 512], 0 is background
+    foreground_soft_mask = torch.sum(foreground_predictions, axis=0) # [H, W]
+    foreground_soft_mask = torch.unsqueeze(foreground_soft_mask, axis=-1) # [H, W, 1]
+    foreground_soft_mask = torch.cat(
+        (foreground_soft_mask, foreground_soft_mask, foreground_soft_mask),
+        axis=-1
+    )
+    foreground_soft_mask = foreground_soft_mask.cpu().numpy()
+    foreground_soft_mask = (foreground_soft_mask * 255).astype(np.uint8)
+    cv2.imwrite(os.path.join(output_path, 'guided_mask.png'), foreground_soft_mask)
 
 
 if __name__ == "__main__":
@@ -254,22 +299,12 @@ if __name__ == "__main__":
         use_gpu = False
         raise RuntimeError("must use the gpu!!!!")
 
-    path = opts.img_path
-    I_1_path = os.path.join(path, 'I_1.png')
-    synth_path = os.path.join(path, 'project.png')
 
     inference(
         net=net,
-        img_path=I_1_path,
-        output_path=path,
-        output_name='I_1_M.png',
+        img_path=opts.img_path,
+        output_path='outputs',
         use_gpu=use_gpu,
     )
 
-    inference(
-        net=net,
-        img_path=synth_path,
-        output_path=path,
-        output_name='project_M.png',
-        use_gpu=use_gpu,
-    )
+    
